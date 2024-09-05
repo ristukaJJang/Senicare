1d07acdfa9c77bf4e4d1692272b55a4aa5cc11b1
ristukaJJang <ghwjd5585@gmail.com>
2024-09-05 12:45:06 +0900

    09-05 12:45

---
+++ src/constants/index.ts
@@ -0,0 +1,38 @@
+// variable: 상대 경로 상수 //
+export const ROOT_PATH = '/';
+
+export const AUTH_PATH = '/auth';
+
+export const CS_PATH = '/cs';
+export const CS_WRITE_PATH = 'write';
+export const CS_DETAIL_PATH = (customNumber: String | number) => `${customNumber}`;
+export const CS_UPDATE_PATH =  (customNumber: String | number) => `${customNumber}/update`;
+
+export const MM_PATH = '/mm';
+
+export const HR_PATH = '/hr';
+export const HR_DETAIL_PATH = (userId: String) => `${userId}`;
+export const HR_UPDATE_PATH = (userId: String) => `${userId}/update`; 
+
+export const OTHERS_PATH = '*';
+
+// variable: 절대 경로 상수 //
+export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
+
+export const AUTH_ABSOLUTE_PATH = AUTH_PATH;
+
+export const CS_ABSOLUTE_PATH = CS_PATH;
+export const CS_ABSOLUTE_WRITE_PATH = `${CS_PATH}/${CS_WRITE_PATH}`;
+export const CS_DETAIL_ABSOLUTE_PATH = (customNumber: String | number) => `${CS_PATH}/${CS_DETAIL_PATH(customNumber)}`;
+export const CS_UPDATE_ABSOLUTE_PATH =  (customNumber: String | number) => `${CS_PATH}/${CS_UPDATE_PATH(customNumber)}`;
+
+export const MM_ABSOLUTE_PATH = MM_PATH;
+
+export const HR_ABSOLUTE_PATH = HR_PATH;
+export const HR_DETAIL_ABSOLUTE_PATH = (userId: String) => `${HR_PATH}/${HR_DETAIL_PATH(userId)}`;
+export const HR_UPDATE_ABSOLUTE_PATH = (userId: String) => `${HR_PATH}/${HR_UPDATE_PATH(userId)}`; 
+
+export const OTHERS_ABSOLUTE_PATH = OTHERS_PATH;
+
+// variable: HTTP BEARER TOKEN COOKIE NAME //
+export const ACCESS_TOKEN = 'accessToken';
